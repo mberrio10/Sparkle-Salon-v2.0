7159abd0cde5a8103d311d53c38893bc7201829c
Mike Berrio <mikeberrio.a@gmail.com>
2025-10-18 14:02:25 -0400

    chore: update express to latest and remove body-parser

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
 const pagesRouter = require("./routes/pages"); // import the modularized routes
 const dotenv = require("dotenv");
 
